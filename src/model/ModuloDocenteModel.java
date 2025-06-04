811d0d21bbb607dcb5c25d4ddae7ef7d61679e54
MiguelACastro <mcastro_23@alu.uabcs.mx>
2025-06-04 12:05:43 -0700

    Avance CRUD docentes: Insertar nuevo registro

--- src/model/ModuloDocenteModel.java
+++ src/model/ModuloDocenteModel.java
@@ -5,10 +5,13 @@ import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 
 import javax.imageio.ImageIO;
+
+import model.exception.UniqueKeyViolationException;
 
 public class ModuloDocenteModel {
 	
@@ -47,5 +50,42 @@ public class ModuloDocenteModel {
 			e.printStackTrace();
 		}
 		return docentes;
+	}
+	
+	public boolean add(Docente docente) throws UniqueKeyViolationException {
+		String query = "INSERT INTO teachers (first_name, last_name, gender, birthday, "
+				+ 				"phone_number, email, degree, "
+				+ "				curp, address, picture)"
+				+ "		VALUES (?,?,?,?,?,?,?,?,?,?)";
+		
+		try (
+				Connection conn = ConexionBD.getConnection();
+				PreparedStatement stmt = conn.prepareStatement(query);
+			){
+			stmt.setString(1, docente.getNombres());
+			stmt.setString(2, docente.getApellidos());
+			stmt.setString(3, docente.getGenero());
+			java.sql.Date fechaConvertida =  new java.sql.Date(docente.getFechaNacimiento().getTime());
+			stmt.setDate(4, fechaConvertida);
+			stmt.setString(5, docente.getTelefono());
+			stmt.setString(6, docente.getCorreo());
+			stmt.setString(7, docente.getGradoEstudios());
+			stmt.setString(8, docente.getCurp());
+			stmt.setString(9, docente.getDomicilio());
+			stmt.setBytes(10, Utils.toByte(Utils.toBufferedImage(docente.getFoto())));
+			
+			int rs = stmt.executeUpdate();
+			if(rs>0) {
+				return true;
+			}
+		} catch (SQLException e) {
+			if(e.getErrorCode() == 1062) {
+				//Se introdujo un email o CURP que ya existe
+				throw UniqueKeyViolationException.fromSQLException(e);
+			}
+			e.printStackTrace();
+			return false;
+		}
+		return false;
 	}
 }

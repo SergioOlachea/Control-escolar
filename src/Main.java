e55c5cf5a53f3e9174e44ffc6d7ebb276b1b324e
MiguelACastro <mcastro_23@alu.uabcs.mx>
2025-06-11 06:46:12 -0700

    Cambiar Look and Feel

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
+import java.awt.Color;
+
+import javax.swing.UIManager;
+
+import com.formdev.flatlaf.FlatLightLaf;
+
 import controlles.Controller;
 
 public class Main {
 
 	public static void main(String[] args) {
+		FlatLightLaf.setup();
+		UIManager.put("Table.showVerticalLines", true);
+		UIManager.put("Table.showHorizontalLines", true);
+		UIManager.put("Table.gridColor", Color.LIGHT_GRAY);
+		UIManager.put("TableHeader.background", Color.LIGHT_GRAY);
 		Controller control = new Controller();
 		control.despliegue();
-
 	}
 
 }
